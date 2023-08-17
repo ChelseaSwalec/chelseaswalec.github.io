01ebc65ddbd2338f3d87d0533d610d33f7c467f5
Alexander J. Woods <26715038+alexanderjwoods@users.noreply.github.com>
2023-08-17 06:04:54 -0400

    Update experience

--- code/site/src/app/components/experience/experience.component.ts
+++ code/site/src/app/components/experience/experience.component.ts
@@ -9,9 +9,9 @@ import { Experience } from 'src/app/models/experience';
 export class ExperienceComponent {
   public title:string = "Experience";
   public elements: Experience[] = new Array<Experience>(
-    new Experience("Shipping Lead", "Vibra-Tite", "", "June 2019", "Present"),
-    new Experience("Shipping Clerk", "Vibra-Tite", "", "June 2015", "June 2019"),
-    new Experience("Data Entry Clerk", "Vibra-Tite", "", "June 2014", "June 2015"),
-    new Experience("Production Team Member", "Vibra-Tite", "", "June 2013", "June 2014")
+    new Experience("Shipping Lead", "Vibra-Tite", new Array<string>("Organizing and distributing daily workload", "Maintaining EDI data accuracy", "Handling and packaging of restricted hazardous materials", "Managing all DGIS reporting", "Performing logistics analysis to optimize shipping throughput", "Performing evaluations of team members as needed"), "June 2019", "Present"),
+    new Experience("Shipping Clerk", "Vibra-Tite", new Array<string>("Packing orders for shipping", "Creating labels for orders", "Maintaining inventory accuracy"), "June 2015", "June 2019"),
+    new Experience("Data Entry Clerk", "Vibra-Tite", new Array<string>("Entering data in various systems", "Ensuring data accuracy and validity"), "June 2014", "June 2015"),
+    new Experience("Production Team Member", "Vibra-Tite", new Array<string>("Operating small machines in the production of various products", "Inspecting products for quality"), "June 2013", "June 2014")
   );
 }
--- code/site/src/app/models/experience.ts
+++ code/site/src/app/models/experience.ts
@@ -1,11 +1,11 @@
 export class Experience {
     public title:string;
     public company:string;
-    public description:string;
+    public description:string[];
     public startYear:string;
     public endYear:string;
 
-    constructor(title:string, company:string, description:string, startYear:string, endYear:string) {
+    constructor(title:string, company:string, description:string[], startYear:string, endYear:string) {
         this.title = title;
         this.company = company;
         this.description = description;
